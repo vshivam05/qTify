a44506026eb1f6be7a6ddbbbaaed2f8383faaf11
vshivam05 <sv699291@gmail.com>
2024-08-29 13:48:25 +0530

    commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,42 +1,42 @@
 
-// import styles from "./Hero.module.css";
-// import heroImage from "../../assets/hero.png";
-
-// const Hero = () => {
-//   return (
-//     <div className={styles.hero}>
-//       <div>
-//         <h1> 100 Thousand Songs, ad - free </h1>
-//         <h1> Over thousands podcast episodes</h1>
-//       </div>
-//       <img src={heroImage} alt="Hero_Image" />
-//     </div>
-//   );
-// };
-
-// export default Hero;
-
-import React from "react";
 import styles from "./Hero.module.css";
-// import heroImage from "../../assets/hero.png";
+import heroImage from "../../assets/hero.png";
 
-function Hero() {
+const Hero = () => {
   return (
     <div className={styles.hero}>
       <div>
         <h1>100 Thousand Songs, ad-free</h1>
         <h1>Over thousands podcast episodes</h1>
       </div>
-      <div>
-        <img
-          src={require("../../assets/hero_headphones.png")}
-          width={212}
-          alt="headphones"
-        />
-      </div>
+      <img src={heroImage} alt="Hero_Image" />
     </div>
   );
-}
+};
 
 export default Hero;
+
+// import React from "react";
+// import styles from "./Hero.module.css";
+// import heroImage from "../../assets/hero.png";
+
+// function Hero() {
+//   return (
+//     <div className={styles.hero}>
+//       <div>
+//         <h1>100 Thousand Songs, ad-free</h1>
+//         <h1>Over thousands podcast episodes</h1>
+//       </div>
+//       <div>
+//         <img
+//           src={heroImage}
+//           width={212}
+//           alt="headphones"
+//         />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Hero;
 
